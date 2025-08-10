750a84a8e324a530ae0bf733387e2025bc964cf7
Tomkoid <tomaszierl@outlook.com>
2025-08-10 19:37:48 +0200

    chore: improve gradient
    Signed-off-by: Tomkoid <tomaszierl@outlook.com>

--- src/lib/components/NavbarSection.svelte
+++ src/lib/components/NavbarSection.svelte
@@ -1,6 +1,6 @@
 <div
 	class="w-fit bg-gray-800 hover:bg-gray-700
-	transition-all duration-500 relative z-10 overflow-hidden bg-gradient-to-b from-blue-900 from-40% to-black
+	transition-all duration-500 relative z-10 overflow-hidden bg-gradient-to-r from-blue-900 from-40% to-teal-800
 before:absolute before:rounded-full before:top-0 before:left-0
 before:w-full before:h-full
 before:bg-gradient-to-r before:from-primary before:to-purple-900
