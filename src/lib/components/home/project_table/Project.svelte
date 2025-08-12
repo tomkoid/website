2da4e6d5e9e10d2e482d02bd6d2e8c865a4f90ba
Tomkoid <tomaszierl@outlook.com>
2025-08-12 18:06:32 +0200

    feat: add images to projects
    Signed-off-by: Tomkoid <tomaszierl@outlook.com>

--- src/lib/components/home/project_table/Project.svelte
+++ src/lib/components/home/project_table/Project.svelte
@@ -4,30 +4,43 @@
 	export let name: string;
 	export let description: string;
 	export let link: string = '';
+	export let logoPath: string = '/logo.png';
 </script>
 
 <div
-	class="transition-all p-6 rounded-2xl bg-gray-800/50 backdrop-blur-md border border-gray-800 hover:border-gray-600"
+	class="flex flex-col xl:flex-row flex-nowrap gap-6 transition-all p-6 rounded-2xl bg-gray-800/50 backdrop-blur-md border border-gray-800 hover:border-gray-600"
 >
-	<div class="text-2xl font-bold mb-2">
-		{#if link != ''}
-			<div class="flex flex-row items-center gap-3">
-				<a
-					class="transition-all hover:underline hover:text-purple-300"
-					href={link}
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					{name}
-				</a>
-				<a href={link} target="_blank" rel="noopener noreferrer">
-					<Icon width="24" icon="mdi:open-in-new" />
-				</a>
-			</div>
-		{:else}
-			<h2>{name}</h2>
-		{/if}
+	<div>
+		<a href={link}>
+			<img
+				class="min-w-16 md:min-w-24 xl:min-w-32 w-32 rounded-4xl drop-shadow-3xl border-dashed border-4 border-orange-400/70 hover:border-orange-500 transition-all"
+				id="pfp"
+				src={logoPath}
+				alt="Moje profilova fotka"
+			/>
+		</a>
 	</div>
+	<div>
+		<div class="text-2xl font-bold mb-2">
+			{#if link != ''}
+				<div class="flex flex-row items-center gap-3">
+					<a
+						class="transition-all hover:underline hover:text-purple-300"
+						href={link}
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						{name}
+					</a>
+					<a href={link} target="_blank" rel="noopener noreferrer">
+						<Icon width="24" icon="mdi:open-in-new" />
+					</a>
+				</div>
+			{:else}
+				<h2>{name}</h2>
+			{/if}
+		</div>
 
-	<p>{description}</p>
+		<p>{description}</p>
+	</div>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -77,31 +77,37 @@
 				name="Stravule"
 				description="Automatic food picker for strava.cz that selects meals based on user-defined filters"
 				link="https://codeberg.org/tomkoid/stravule"
+				logoPath="/logos/stravule.png"
 			/>
 			<Project
 				name="reŠOL"
 				description="A modern, privacy-focused web app improving the Škola OnLine experience"
 				link="https://codeberg.org/resol/resol"
+				logoPath="/logos/resol-logo.png"
 			/>
 			<Project
 				name="Image Chat"
 				description="An open-source application that uses AI to analyze photos and describes their contents using text-to-speech technology (2024 AimtecHackathon winning project)"
 				link="https://gitlab.com/danielzierl/image-chat"
-			/>
-			<Project
-				name="Blokator"
-				description="System-wide CLI adblocker for Linux, MacOS and Windows using the system's hosts file"
-				link="https://codeberg.org/tomkoid/blokator"
-			/>
-			<Project
-				name="mdhtml"
-				description="Really simple CLI Markdown to HTML converter with styling support"
-				link="https://codeberg.org/tomkoid/mdhtml"
+				logoPath="/logos/image-chat.png"
 			/>
 			<Project
 				name="Final Trial"
 				description="A simple game made in Godot engine that runs on Android, PC and web. Your goal is to bounce the good guys to Heaven and let the bad ones fall to Hell"
 				link="https://codeberg.org/dystopia/final-trial"
+				logoPath="/logos/final-trial.png"
+			/>
+			<Project
+				name="Blokator"
+				description="System-wide CLI adblocker for Linux, MacOS and Windows using the system's hosts file"
+				link="https://codeberg.org/tomkoid/blokator"
+				logoPath="/logos/blokator.png"
+			/>
+			<Project
+				name="mdhtml"
+				description="Really simple CLI Markdown to HTML converter with styling support"
+				link="https://codeberg.org/tomkoid/mdhtml"
+				logoPath="/logos/mdhtml.png"
 			/>
 		</ProjectTable>
 	</section>
