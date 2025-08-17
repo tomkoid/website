05dd864ab0bc42613e63bd2382e9525492eee8f1
Tomkoid <tomaszierl@outlook.com>
2025-08-17 14:37:52 +0200

    chore: convert project logos to webp
    Signed-off-by: Tomkoid <tomaszierl@outlook.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -81,37 +81,37 @@
 				name="Stravule"
 				description="Automatic food picker for strava.cz that selects meals based on user-defined filters"
 				link="https://codeberg.org/tomkoid/stravule"
-				logoPath="/logos/stravule.png"
+				logoPath="/logos/stravule.webp"
 			/>
 			<Project
 				name="reŠOL"
 				description="A modern, privacy-focused web app improving the Škola OnLine experience"
 				link="https://codeberg.org/resol/resol"
-				logoPath="/logos/resol-logo.png"
+				logoPath="/logos/resol-logo.webp"
 			/>
 			<Project
 				name="Image Chat"
 				description="An open-source application that uses AI to analyze photos and describes their contents using text-to-speech technology (2024 AimtecHackathon winning project)"
 				link="https://gitlab.com/danielzierl/image-chat"
-				logoPath="/logos/image-chat.png"
+				logoPath="/logos/image-chat.webp"
 			/>
 			<Project
 				name="Final Trial"
 				description="A simple game made in Godot engine that runs on Android, PC and web. Your goal is to bounce the good guys to Heaven and let the bad ones fall to Hell"
 				link="https://codeberg.org/dystopia/final-trial"
-				logoPath="/logos/final-trial.png"
+				logoPath="/logos/final-trial.webp"
 			/>
 			<Project
 				name="Blokator"
 				description="System-wide CLI adblocker for Linux, MacOS and Windows using the system's hosts file"
 				link="https://codeberg.org/tomkoid/blokator"
-				logoPath="/logos/blokator.png"
+				logoPath="/logos/blokator.webp"
 			/>
 			<Project
 				name="mdhtml"
 				description="Really simple CLI Markdown to HTML converter with styling support"
 				link="https://codeberg.org/tomkoid/mdhtml"
-				logoPath="/logos/mdhtml.png"
+				logoPath="/logos/mdhtml.webp"
 			/>
 		</ProjectTable>
 	</section>
