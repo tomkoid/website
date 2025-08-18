2178a8899f89853763770cc891816bbf62e29aaa
Tomkoid <tomaszierl@outlook.com>
2025-08-18 17:49:30 +0200

    feat: home page transition
    Signed-off-by: Tomkoid <tomaszierl@outlook.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,9 +9,16 @@
 	import { onMount } from 'svelte';
 	import { mouseGlowFollow } from '$lib/js/bg';
 	import { ProjectTable, Project } from '$lib/components/home/project_table';
+	import { fly } from 'svelte/transition';
+
+	let visible = $state(false);
 
 	onMount(() => {
 		mouseGlowFollow();
+
+		setTimeout(() => {
+			visible = true;
+		}, 200);
 	});
 </script>
 
@@ -21,6 +28,7 @@
 
 <div class="bg-gradient-to-r from-gray-900 to-zinc-900 min-h-screen h-fit">
 	<!-- class="flex bg-gradient-to-r from-gray-900 to-zinc-900 xl:flex-row flex-col-reverse lg:flex-nowrap justify-center xl:items-left items-center w-full gap-8 xl:gap-16 pt-50 py-30 px-7" -->
+
 	<div
 		class="flex xl:flex-row flex-col-reverse lg:flex-nowrap justify-center xl:items-left items-center w-full gap-8 xl:gap-16 pt-30 md:pt-35 xl:pt-45 pb-30 xl:pb-40 px-7"
 		id="intro-bg"
@@ -70,51 +78,53 @@
 		</div>
 	</div>
 
-	<section class="py-5 pb-20 px-7 max-w-5xl mx-auto">
-		<p class="text-xl text-gray-300 text-center xl:text-left mb-10">
-			Over the years, I’ve worked on several projects. Here are some of the things I’d most like you
-			to see:
-		</p>
+	{#if visible}
+		<section class="py-5 pb-20 px-7 max-w-5xl mx-auto" transition:fly>
+			<p class="text-xl text-gray-300 text-center xl:text-left mb-10">
+				Over the years, I’ve worked on several projects. Here are some of the things I’d most like
+				you to see:
+			</p>
 
-		<ProjectTable>
-			<Project
-				name="Stravule"
-				description="Automatic food picker for strava.cz that selects meals based on user-defined filters"
-				link="https://codeberg.org/tomkoid/stravule"
-				logoPath="/logos/stravule.webp"
-			/>
-			<Project
-				name="reŠOL"
-				description="A modern, privacy-focused web app improving the Škola OnLine experience"
-				link="https://codeberg.org/resol/resol"
-				logoPath="/logos/resol-logo.webp"
-			/>
-			<Project
-				name="Image Chat"
-				description="An open-source application that uses AI to analyze photos and describes their contents using text-to-speech technology (2024 AimtecHackathon winning project)"
-				link="https://gitlab.com/danielzierl/image-chat"
-				logoPath="/logos/image-chat.webp"
-			/>
-			<Project
-				name="Final Trial"
-				description="A simple game made in Godot engine that runs on Android, PC and web. Your goal is to bounce the good guys to Heaven and let the bad ones fall to Hell"
-				link="https://codeberg.org/dystopia/final-trial"
-				logoPath="/logos/final-trial.webp"
-			/>
-			<Project
-				name="Blokator"
-				description="System-wide CLI adblocker for Linux, MacOS and Windows using the system's hosts file"
-				link="https://codeberg.org/tomkoid/blokator"
-				logoPath="/logos/blokator.webp"
-			/>
-			<Project
-				name="mdhtml"
-				description="Really simple CLI Markdown to HTML converter with styling support written in Go"
-				link="https://codeberg.org/tomkoid/mdhtml"
-				logoPath="/logos/mdhtml.webp"
-			/>
-		</ProjectTable>
-	</section>
+			<ProjectTable>
+				<Project
+					name="Stravule"
+					description="Automatic food picker for strava.cz that selects meals based on user-defined filters"
+					link="https://codeberg.org/tomkoid/stravule"
+					logoPath="/logos/stravule.webp"
+				/>
+				<Project
+					name="reŠOL"
+					description="A modern, privacy-focused web app improving the Škola OnLine experience"
+					link="https://codeberg.org/resol/resol"
+					logoPath="/logos/resol-logo.webp"
+				/>
+				<Project
+					name="Image Chat"
+					description="An open-source application that uses AI to analyze photos and describes their contents using text-to-speech technology (2024 AimtecHackathon winning project)"
+					link="https://gitlab.com/danielzierl/image-chat"
+					logoPath="/logos/image-chat.webp"
+				/>
+				<Project
+					name="Final Trial"
+					description="A simple game made in Godot engine that runs on Android, PC and web. Your goal is to bounce the good guys to Heaven and let the bad ones fall to Hell"
+					link="https://codeberg.org/dystopia/final-trial"
+					logoPath="/logos/final-trial.webp"
+				/>
+				<Project
+					name="Blokator"
+					description="System-wide CLI adblocker for Linux, MacOS and Windows using the system's hosts file"
+					link="https://codeberg.org/tomkoid/blokator"
+					logoPath="/logos/blokator.webp"
+				/>
+				<Project
+					name="mdhtml"
+					description="Really simple CLI Markdown to HTML converter with styling support written in Go"
+					link="https://codeberg.org/tomkoid/mdhtml"
+					logoPath="/logos/mdhtml.webp"
+				/>
+			</ProjectTable>
+		</section>
+	{/if}
 </div>
 
 <!-- <h1>Welcome to SvelteKit</h1> -->
