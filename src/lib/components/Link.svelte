9ed864cef85853b4e6e2889f96ee3edb74dd4b51
Tomkoid <tomaszierl@outlook.com>
2025-09-05 10:05:01 +0200

    feat: add about me section
    Signed-off-by: Tomkoid <tomaszierl@outlook.com>

---
+++ src/lib/components/Link.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	export let href: string = '';
+	export let target: string = '';
+</script>
+
+<a
+	class="text-blue-300 hover:text-blue-200 transition-all underline"
+	{href}
+	{target}
+	rel="noopener noreferrer"
+>
+	<slot />
+</a>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,6 +10,7 @@
 	import { mouseGlowFollow } from '$lib/js/bg';
 	import { ProjectTable, Project } from '$lib/components/home/project_table';
 	import { fly } from 'svelte/transition';
+	import Link from '$lib/components/Link.svelte';
 
 	let visible = $state(false);
 
@@ -124,6 +125,26 @@
 				/>
 			</ProjectTable>
 		</section>
+
+		<div class="px-7 max-w-5xl mx-auto pb-25">
+			<h1 class="text-4xl font-bold mb-4 flex justify-center underline">About me</h1>
+			<div class="flex flex-wrap flex-col gap-3 text-xl">
+				<p>
+					I'm <b>a software engineer</b> and <b>an advocate of FOSS software</b> and when I'm not programming
+					I usually learn what I find interesting at the moment.
+				</p>
+				<p>
+					Currently I use <Link href="https://go.dev">Go</Link> and
+					<Link href="https://www.rust-lang.org">Rust</Link> for most of my projects. Also, I enjoy playing
+					with
+					<Link href="https://wikiless.tiekoetter.com/wiki/Linux?lang=en">Linux</Link>, especially
+					with
+					<Link href="https://wikiless.tiekoetter.com/wiki/Tiling_window_manager?lang=en"
+						>tiling window managers</Link
+					>, text editors and random cool tools.
+				</p>
+			</div>
+		</div>
 	{/if}
 </div>
 
